928e08e8e634fbc0b11d22f5960c29d60cdb20f5
fnareoh <garance.gourdel@gmail.com>
2019-02-04 10:25:25 +0100

    fix simulated annealing

--- src/main.cpp
+++ src/main.cpp
@@ -38,9 +38,8 @@ int main()
                 new LocalSearch(300, seed, 1000, true),
                 new ExampleOpt(300, seed, 10000),
                 new CorrMax(300, seed, 10000, 1),
-                new Simulated_Annealing(300, seed, 5000, 0.1)
-
-                //new Genetic(300, seed, 300, 1000, 3000)
+                new Simulated_Annealing(300, seed, 10000, 0.013)
+                new Genetic(300, seed, 300, 1000, 3000)
             }
         },
         // Increasing dimention
@@ -65,6 +64,13 @@ int main()
                 {"abciss", "nb_parents"}
             },
             {}
+        },
+        {
+            {
+                {"type", "benchmark"},
+                {"abciss", "threshold"}
+            },
+            {}
         }
 
     };
@@ -72,8 +78,8 @@ int main()
     for (int n = 20 ; n <= 400 ; n += 20) {
         tests[1].second.push_back(new CorrMax(n, seed, 5000, 1));
         tests[1].second.push_back(new ExampleOpt(n, seed, 5000));
-        tests[1].second.push_back(new Simulated_Annealing(n, seed, 5000, 0.1));
-        //tests[1].second.push_back(new Genetic(n, seed, 100, 300, 3000));
+        tests[1].second.push_back(new Simulated_Annealing(n, seed, 5000, 0.013));
+        tests[1].second.push_back(new Genetic(n, seed, 100, 300, 3000));
     }
 
     /*
@@ -86,6 +92,9 @@ int main()
         tests[3].second.push_back(new Genetic(300, seed, 100, n, 3000));
     }
     */
+    for (int n = 500 ; n >= 20 ; n -= 10) {
+        tests[4].second.push_back(new  Simulated_Annealing(300, seed, 5000, (double) 1/n));
+    }
 
     run_output_tests(ostream, tests, 10);
 }
--- src/optimizers/simulated_annealing.cpp
+++ src/optimizers/simulated_annealing.cpp
@@ -6,6 +6,7 @@ Simulated_Annealing::Simulated_Annealing(int seq_size, int seed, int nb_iter, do
 {
     name = "simulated_annealing";
     params = {
+        {"seed", (int) seed},
         {"iterations", (int) nb_iter},
         {"threshold" , (double) threshold}
     };
@@ -14,18 +15,27 @@ Simulated_Annealing::Simulated_Annealing(int seq_size, int seed, int nb_iter, do
 
 Sequence Simulated_Annealing::run(LabsInstance& instance)
 {
+    srand (params["seed"]);
     Sequence seq = random_sequence();
     double seq_merit = instance.eval(seq);
+    Sequence best_seq = seq;
+    double best_merit = seq_merit;
 
     for (int step = 0 ; step < params["iterations"] ; step++) {
         int rand_bit = rand() % seq_size;
         Sequence new_seq = seq;
         new_seq[rand_bit] = -seq[rand_bit];
         double new_seq_merit = instance.eval(new_seq);
-        if (new_seq_merit >= seq_merit - params["threshold"]){
+
+        if (new_seq_merit >= seq_merit ||
+            (new_seq_merit >= seq_merit - params["threshold"] && step < params["iterations"]*0.75)){
             seq = new_seq;
             seq_merit = new_seq_merit;
         }
+        if (seq_merit > best_merit){
+            best_seq = seq;
+            best_merit = seq_merit;
+        }
     }
-    return seq;
+    return best_seq;
 }
