860bf06127d1468171bd6d03579936f456aa4f3f
fnareoh <garance.gourdel@gmail.com>
2019-02-08 10:51:04 +0100

    add graph in nb eval

--- plot/main.py
+++ plot/main.py
@@ -9,17 +9,18 @@ tests_set = json.loads(file_tests.read())
 
 def compare_algo(tests):
     dimension_dict = {}
-    fig = plt.figure()
     for test in tests:
         if test['dim'] not in dimension_dict.keys():
-            dimension_dict[test['dim']] = {'name':[],'running_time':[],'merit':[]}
+            dimension_dict[test['dim']] = {'name':[],'running_time':[], 'nb_evaluation':[],'merit':[]}
         name = test['name']
         for p in test['params']:
             name = name + ' ' + '\n' + p + '=' + str(test['params'][p])
         dimension_dict[test['dim']]['name'].append(name)
         dimension_dict[test['dim']]['running_time'].append(test['mean_time'])
+        dimension_dict[test['dim']]['nb_evaluation'].append(max(test['nb_requests'],test['nb_atomic_swaps']))
         dimension_dict[test['dim']]['merit'].append(test['mean_merit'])
     for N in dimension_dict.keys():
+        fig = plt.figure()
         for i in range(len(dimension_dict[N]['merit'])):
             plt.scatter(dimension_dict[N]['running_time'][i],
             dimension_dict[N]['merit'][i])
@@ -32,6 +33,19 @@ def compare_algo(tests):
         #plt.show()
         fig.savefig('figure/compare_'+str(N)+'.png', dpi=fig.dpi, bbox_inches='tight')
         fig.savefig('figure/compare_'+str(N)+'.pdf', bbox_inches='tight')
+        fig = plt.figure()
+        for i in range(len(dimension_dict[N]['merit'])):
+            plt.scatter(dimension_dict[N]['nb_evaluation'][i],
+            dimension_dict[N]['merit'][i])
+            plt.annotate(dimension_dict[N]['name'][i],
+            (dimension_dict[N]['nb_evaluation'][i],
+            dimension_dict[N]['merit'][i]))
+        plt.xlabel('number of evaluation')
+        plt.ylabel('merit')
+        #fig.suptitle('Comparison in results and runtime of the different algorithm')
+        #plt.show()
+        fig.savefig('figure/compare_nb_eval_'+str(N)+'.png', dpi=fig.dpi, bbox_inches='tight')
+        fig.savefig('figure/compare_nb_eval_'+str(N)+'.pdf', bbox_inches='tight')
 
 def convergence_in_nb_step(tests):
     step_tests = [x  for x in tests if x['steps'] != {}]
@@ -57,10 +71,11 @@ def evolution_in_dimension(tests):
     algo_dict = {}
     for test in tests:
         if test['name'] not in algo_dict.keys():
-            algo_dict[test['name']] = {'dimension':[],'running_time':[],'mean_merit':[],'max_merit':[]}
+            algo_dict[test['name']] = {'dimension':[],'running_time':[],'nb_evaluation':[], 'mean_merit':[],'max_merit':[]}
         else:
             algo_dict[test['name']]['dimension'].append(test['dim'])
             algo_dict[test['name']]['running_time'].append(test['mean_time'])
+            algo_dict[test['name']]['nb_evaluation'].append(max(test['nb_requests'],test['nb_atomic_swaps']))
             algo_dict[test['name']]['mean_merit'].append(test['mean_merit'])
             algo_dict[test['name']]['max_merit'].append(test['merit'])
     fig = plt.figure()
@@ -90,6 +105,15 @@ def evolution_in_dimension(tests):
     #fig.suptitle('Evolution of the mean-runtime of the algorithms ' + '\n depending on the dimension', fontsize=12)
     fig.savefig('figure/dimension_runtime'+'.png', dpi=fig.dpi, bbox_inches='tight')
     fig.savefig('figure/dimension_runtime'+'.pdf', dpi=fig.dpi, bbox_inches='tight')
+    fig = plt.figure()
+    for algo in algo_dict.keys():
+        plt.plot(algo_dict[algo]['dimension'],algo_dict[algo]['nb_evaluation'], label=algo)
+    plt.xlabel('dimension')
+    plt.ylabel('number of evaluation')
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.12))
+    #fig.suptitle('Evolution of the number of evaluation of the algorithms ' + '\n depending on the dimension', fontsize=12)
+    fig.savefig('figure/dimension_eval'+'.png', dpi=fig.dpi, bbox_inches='tight')
+    fig.savefig('figure/dimension_eval'+'.pdf', dpi=fig.dpi, bbox_inches='tight')
 
 def evolution_in_param(param, tests):
     param_dict = {param : [], 'running_time' : [], 'merit' : []}
@@ -105,8 +129,8 @@ def evolution_in_param(param, tests):
     plt.ylabel('merit')
     plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.12))
     #fig.suptitle('Evolution of the merit of algorithm ' + name + '\n depending on the ' + param, fontsize=12)
-    fig.savefig('figure/param_merit_' + name + '_' + param + '.png', dpi=fig.dpi, bbox_inches='tight')
-    fig.savefig('figure/param_merit_' + name + '_' + param + '.pdf', dpi=fig.dpi, bbox_inches='tight')
+    fig.savefig('figure/param_mean_merit_' + name + '_' + param + '.png', dpi=fig.dpi, bbox_inches='tight')
+    fig.savefig('figure/param_mean_merit_' + name + '_' + param + '.pdf', dpi=fig.dpi, bbox_inches='tight')
     fig = plt.figure()
     plt.plot(param_dict[param],param_dict['running_time'], label=param)
     plt.xlabel(param)
